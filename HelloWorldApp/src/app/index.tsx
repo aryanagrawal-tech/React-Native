12e5a38b3eab4cd0feeb6354448d23a01c567580
aryanagrawal-tech <agrawalaryan0910@gmail.com>
2026-09-09 21:20:49 +0530

    Hello World in React Native

---
+++ HelloWorldApp/src/app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default App;
